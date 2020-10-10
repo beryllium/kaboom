1513e85d74374ab7a1a0a1be9cccace6a1d12751
Kevin Boyd <kevin.boyd@gmail.com>
2020-10-10 12:07:55 -0700

    Additional comments

--- Kaboom.php
+++ Kaboom.php
@@ -2,9 +2,14 @@
 
 namespace Beryllium\Kaboom;
 
+/**
+ * Enables force feedback in your code. In a manner of speaking.
+ */
 class Kaboom
 {
     /**
+     * Throw an exception when a custom condition is tripped
+     *
      * @param string   $message
      * @param callable $condition
      *
@@ -19,6 +24,15 @@ class Kaboom
         throw new KaboomException($message);
     }
 
+    /**
+     * Throw an exception once the provided $date has been passed
+     *
+     * @param string $message   The message to embed in the exception
+     * @param string $date      A date string compatible with strftime()
+     *
+     * @return bool
+     * @throws KaboomException
+     */
     public function todo(string $message, string $date): bool {
         $timestamp = strtotime($date);
 
